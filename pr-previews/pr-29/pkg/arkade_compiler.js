cddf1849dda0470e824a662d4e0150d0fa53c1b0
msinkec <msinkec@users.noreply.github.com>
2026-07-06 12:45:45 +0000

    Deploying to gh-pages from @ arkade-os/compiler@5aeb2aa4f846161649ab96dd07a6e057751c67c2 🚀

--- pr-previews/pr-29/pkg/arkade_compiler.js
+++ pr-previews/pr-29/pkg/arkade_compiler.js
@@ -79,7 +79,7 @@ export function version() {
 function __wbg_get_imports() {
     const import0 = {
         __proto__: null,
-        __wbg___wbindgen_throw_bbadd78c1bac3a77: function(arg0, arg1) {
+        __wbg___wbindgen_throw_344f42d3211c4765: function(arg0, arg1) {
             throw new Error(getStringFromWasm0(arg0, arg1));
         },
         __wbg_error_a6fa202b58aa1cd3: function(arg0, arg1) {
@@ -93,11 +93,11 @@ function __wbg_get_imports() {
                 wasm.__wbindgen_free(deferred0_0, deferred0_1, 1);
             }
         },
-        __wbg_getTime_ef2e115ede346a26: function(arg0) {
+        __wbg_getTime_d6f070c088c9b5ed: function(arg0) {
             const ret = arg0.getTime();
             return ret;
         },
-        __wbg_new_0_878c6ec3a1c600f3: function() {
+        __wbg_new_0_3da9e97f24fc69be: function() {
             const ret = new Date();
             return ret;
         },
